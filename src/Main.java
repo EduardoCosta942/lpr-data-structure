38edd8ee88e7381a173a7b0397c21b004defd276
Eduardo Costa <eduardocosta942z@gmail.com>
2025-08-31 18:38:53 -0300

    feat: main for BalancedExpression.java, includes: menu

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,48 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import Practices.BalancedExpression;
+
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // Declare variables
+        Scanner scanner = new Scanner(System.in);
+        int option;
 
+       // Menu
+        do {
+            // define and validate var option
+            option = 0;
+            menu();
+            try{
+                option = scanner.nextInt();
+            } catch (InputMismatchException ime){
+                ime.printStackTrace();
+            }
+
+            // Actions according to option
+            switch (option){
+                case 1 -> {
+                    System.out.println("""
+                            ---------------------------
+                            What is the expression?
+                            """);
+                    scanner.nextLine(); // Clear buffer
+                    System.out.println(BalancedExpression.isBalanced(scanner.nextLine()));
+                }
+
+                case 2 -> {
+                    option = -1;
+                }
+            }
+        } while (option != -1);
+    }
+    public static  void menu(){
+        System.out.println("""
+                ----------MENU----------
+                [1] isBalanced(String expression)
+                [2] Leave
+                """);
     }
 }
