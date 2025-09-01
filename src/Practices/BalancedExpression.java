f2e79f0eb2c97b2346f6dd56d9710bf69c934cd6
Eduardo Costa <eduardocosta942z@gmail.com>
2025-09-01 09:54:17 -0300

    chore: simplified code for BalancedExpression.java

--- src/Practices/BalancedExpression.java
+++ src/Practices/BalancedExpression.java
@@ -5,21 +5,20 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class BalancedExpression {
-    public static boolean isBalanced(String expression) throws NullPointerException{
+    public static boolean isBalanced(String expression) {
         // Declare variables
-        char[] OPENDELIMITERS = new char[]{'(', '{', '['};
-        char[] CLOSEDELIMITERS = new char[]{')', '}', ']'};
+        char[][] DELIMITERS = {{'(', '{', '['}, {')', '}', ']'}};
         char[] expressionArray;
         Stack<Character> delimiterStack = new Stack<>();
 
         // Traverse the expression
         expressionArray = expression.toCharArray();
         for (int x = 0; x < expressionArray.length; x++){
-            // Traverse constants OPENDELIMITERS and CLOSEDELIMITERS
-            for (int y = 0; y < OPENDELIMITERS.length; y++){
-                if (OPENDELIMITERS[y] == expressionArray[x]) delimiterStack.add(expressionArray[x]); // Add at stack the opening delimiter
-                if(CLOSEDELIMITERS[y] == expressionArray[x]){
-                    if(delimiterStack.peek() == OPENDELIMITERS[y]) delimiterStack.pop(); // Pop the delimiter if the inverted delimiter is on top of the stack
+            // Traverse constants DELIMITERS
+            for (int y = 0; y < DELIMITERS[0].length; y++){
+                if (DELIMITERS[0][y] == expressionArray[x]) delimiterStack.add(expressionArray[x]); // Add at stack the opening delimiter
+                if(DELIMITERS[1][y] == expressionArray[x]){
+                    if(delimiterStack.peek() == DELIMITERS[0][y]) delimiterStack.pop(); // Pop the delimiter if the inverted delimiter is on top of the stack
                 }
             }
         }
